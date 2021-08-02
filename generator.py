250b307b551c183a51c02271ce9c6897075a4cd0
YasinRezvani <yasinrezvani2025@gmail.com>
2021-08-03 00:06:55 +0430

    import qrcode module to file and use qrcode.make() to generate qrcode

--- generator.py
+++ generator.py
@@ -1,3 +1,7 @@
 ﻿import qrcode
 
-
+data= input("Enter yout data for put to QR Code: ")
+img = qrcode.make(data)
+path = input("Enter the location to save QR Code(example --> C:/Usersyasin/Documents): ")
+name = input("Enter Name for QR Code: ") 
+img.save(path +'/'+ name + '.png')
