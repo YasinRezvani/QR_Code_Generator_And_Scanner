900985ecbed2305f929fad0a433b7fc9749b07d8
YasinRezvani <yasinrezvani2025@gmail.com>
2021-08-03 00:25:28 +0430

    use Image.open() to open qr code and use decode() to scan qrcode

--- scanner.py
+++ scanner.py
@@ -1,4 +1,10 @@
 ﻿from pyzbar.pyzbar import decode
 from PIL import Image
 
+img = Image.open(input("Enter Your location of QR Code(example --> C:/Users/yasin/Documents): "))
 
+res = decode(img)
+
+print(res)
+
+# Made By Yasin Rezvani
