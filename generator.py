9b77d4d6c00c2fadb0b0fe26f904204178c05944
YasinRezvani <yasinrezvani2025@gmail.com>
2021-08-03 00:14:17 +0430

    edit comment

--- generator.py
+++ generator.py
@@ -5,3 +5,5 @@ img = qrcode.make(data)
 path = input("Enter the location to save QR Code(example --> C:/Usersyasin/Documents): ")
 name = input("Enter Name for QR Code: ") 
 img.save(path +'/'+ name + '.png')
+
+# Made By Yasin Rezvani
