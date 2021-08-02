0d57fadf12ba090d90c3cc53f38f2d0c03473d08
YasinRezvani <yasinrezvani2025@gmail.com>
2021-08-03 00:35:01 +0430

    edit input()

--- generator.py
+++ generator.py
@@ -1,6 +1,6 @@
 ﻿import qrcode
 
-data= input("Enter yout data for put to QR Code: ")
+data= input("Enter your data for put to QR Code: ")
 img = qrcode.make(data)
 path = input("Enter the location to save QR Code(example --> C:/Usersyasin/Documents): ")
 name = input("Enter Name for QR Code: ") 
